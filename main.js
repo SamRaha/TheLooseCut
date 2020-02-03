e750932e52c8d3cc6b265ab59740f46b655e6ac5
Sam <hstd027@Sams-MacBook-Pro.local>
2020-02-03 18:02:43 +0000

    changed size of pictures and removed picture 11 (promo pic)

--- main.js
+++ main.js
@@ -64,12 +64,12 @@ function easy_background(selector, a1, a2, b1, b2, c1, c2, d1, d2, e1, e2, f1, f
 				document.querySelector(selector).style.backgroundImage = "url('" + e2 + "')";
 			}, delay); // 5
 		}
-		if (e1) {
-			var delay = a1 + b1 + c1 + d1 + e1;
-			setTimeout(function() {
-				document.querySelector(selector).style.backgroundImage = "url('" + f2 + "')";
-			}, delay); // 5
-		}
+		// if (e1) {
+		// 	var delay = a1 + b1 + c1 + d1 + e1;
+		// 	setTimeout(function() {
+		// 		document.querySelector(selector).style.backgroundImage = "url('" + f2 + "')";
+		// 	}, delay); // 5
+		// }
 	}
 
 	slider();
@@ -85,8 +85,6 @@ easy_background(
 	'body',
 	9000,
 	'./assets/background7.jpg',
-	7000,
-	'./assets/background11.jpg',
 	7000,
 	'./assets/background9.jpg',
 	7000,
